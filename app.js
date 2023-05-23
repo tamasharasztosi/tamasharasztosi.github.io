ee508978f2509d48b1ee6247999d875a76abe10c
Tamas Harasztosi <tamasharasztosi@gmail.com>
2023-05-23 09:09:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ function blehandle_double(event, TargetSelector, DataLog) {
   const value = event.target.value.getFloat32(0, true);
   //console.log('DouReceived: ' + value);
   if (value == 0) {
-   TargetSelector.textContent = String("-");
+   TargetSelector.textContent = String("NA");
   } else {
   TargetSelector.textContent = String(Math.round(value * 100) / 100);
   }
